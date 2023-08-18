4081f7073d03820968dacd13c8c0a2001619f3d3
Franck Bloom <bloomfranck@gmail.com>
2023-08-18 14:22:27 +0930

    Changement couleur titre et lien A propos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
     return (
         <div className="header">
-            <NavLink to="/about">A propos</NavLink>
+            <NavLink to="/about" className="inactive-link">A propos</NavLink>
         </div>
     );
 }
